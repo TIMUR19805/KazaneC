998538aac9d08674435d4583055afc197724d94e
TIMUR19805 <tim.bagawiew@yandex.ru>
2024-09-11 10:01:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# "1st program" - арифметика
+print(9 ** 0.5 * 5)
+# "2nd program" - логика
+print(9.99 > 9.98 and 1000 != 1000.1)
+# "3rd program" - школьная загадка
+print(2 * 2 + 2 == 2 * (2 + 2))
+# "4th program" - первый после точки
+print(123.456)
+print(type(float(123.456)))
+print(123.456 * 10)
+print(int(1234.56 % 10))
---
+++ module_1_3..py
@@ -0,0 +1,8 @@
+name = 'Timur'
+print(name)
+age = 44
+print(age)
+age = (age + 7)
+print(age)
+is_student = True
+print(is_student)
